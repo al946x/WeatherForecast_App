e9507c33abae68fdf12719844183a0fb86381065
Alina Ali01~ <alina.zenam@hotmail.com>
2023-01-14 13:43:51 +0000

    html bootstrap cards added

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,35 @@
     - Pull search history from localStorage
     - If search history is not empty, output each city to the search history display in the DOM
 */
-
+// global variables declared 
 var apiKey = '07d2b35b0cfc6919c8c403dd4beaa059';
 var city = 'London';
 var baseURL = 'https://api.openweathermap.org/data/2.5/';
-var weatherURL = baseURL + `weather?&appid=${apiKey}&units=metric&`;
+var currentURL = baseURL + `weather?&appid=${apiKey}&units=metric&`;
 var forecastURL = baseURL + `forecast?&appid=${apiKey}&units=metric&`;
-
-$.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-  .then(function (currentData) {
-    var lon = currentData.coord.lon;
-    var lat = currentData.coord.lat;
-    var cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+var iconURL = 'https://openweathermap.org/img/w/'
+var cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 var foreCastURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=metric`;
+var lon = currentData.coord.lon;
+var lat = currentData.coord.lat;
+var todayIcon = $('.today-wicon');
+var todayDate = $('#today');
+var todayTemp = $('.today-data .temp');
+var todayWind = $('.today-data .wind');
+var todayHumidity = $('.today-data .humidity');
+var forecastWrapper = $('#forecast-container');
+var searchHistory = $('.search-history');
+var localSearches = [];
+var cityListed = $('.city-explored');
+var todayCity = $('.todays-weather h2');
+var emptyContent = $('.empty');
+var contentWeather = $('#content-weather');
+
+
+function inputSubmitted(cityName) {
+$.get(`currentURL + q=${cityName}`)
+  .then(function (currentData) {
+
 
     // console.log(currentData);
     console.log(`
@@ -34,13 +50,18 @@ var foreCastURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiK
       Temp: ${Math.round(currentData.main.temp)} Cº
       Wind: ${currentData.wind.speed} M/S
       Humidity: ${currentData.main.humidity}%
+      IconURL: ${iconURL + currentData.weather[0].icon}.png
     `);
 
-    $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+    $.get(foreCastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`)
       .then(function (forecastData) {
-        console.log(forecastData);
+        for (var castObj of forecastData.list) {
+              console.log(`${iconURL + castObj.weather[0].icon}.png`); 
+        }
+     
       });
   });
+  }
 
   // get users current location 
 
@@ -53,3 +74,5 @@ var foreCastURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiK
   }
   
   getLocation();
+
+  
